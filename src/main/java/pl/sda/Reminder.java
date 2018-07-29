9b6bc6a7e872c42f5865742be2d88f146152a951
Mariusz Krotos <mar.krotos@gmail.com>
2018-07-29 14:51:16 +0200

    zmieniony kod i dodane łączenie listy stringow

--- src/main/java/pl/sda/Reminder.java
+++ src/main/java/pl/sda/Reminder.java
@@ -1,6 +1,7 @@
 package pl.sda;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Reminder {
 
@@ -38,12 +39,13 @@ public class Reminder {
     public static void main(String[] args) {
         streamConvert();
         sortedSetConvert();
+        connect(new ArrayList<>(Arrays.asList("asada", "cos", "tam")));
     }
 
     private static void streamConvert() {
         System.out.println("Input streamConvert: " + Arrays.toString(ALBUMS.toArray()));
 
-        List<Album> favs = new ArrayList<>();
+        /*List<Album> favs = new ArrayList<>();
         for (Album a : ALBUMS) {
             boolean hasFavorite = false;
             for (Track t : a.tracks) {
@@ -54,7 +56,8 @@ public class Reminder {
             }
             if (hasFavorite)
                 favs.add(a);
-        }
+        }*/
+        List<Album> favs = ALBUMS.stream().filter(album -> album.tracks.stream().anyMatch(tr -> (tr.rating > 4))).collect(Collectors.toList());
 
         Collections.sort(favs, (a1, a2) -> a1.name.compareTo(a2.name));
 
@@ -63,9 +66,24 @@ public class Reminder {
 
     private static void sortedSetConvert() {
         List<String> args = Arrays.asList("x", "xxx", "xx", "x");
-        Set<String> s = new HashSet<>();
+        Set<String> s = new TreeSet<>((o1, o2) -> {
+            if (o1.length() > o2.length()) {
+                return -1;
+            } else if (o1.length() == o2.length()) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+        );
         s.addAll(args);
         System.out.println("Result sortedSetConvert: " + s.size() + " distinct words: " + s);
+    }
+
+    private static String connect(List<String> list) {
+        String nowy = list.stream().map(cos -> cos.trim().toUpperCase()).collect(Collectors.joining(" "));
+        System.out.println(nowy);
+        return nowy;
     }
 
 }
